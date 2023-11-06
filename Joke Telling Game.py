a85242409e60da353a417b733fd9bf8e5433b094
Bryson Chen <bjc199@uclive.ac.nz>
2023-11-06 22:41:58 +1300

    Adds "Joke Telling Game.py"

---
+++ Joke Telling Game.py
@@ -0,0 +1,22 @@
+import time
+
+print("What do you get when you cross a snowman and a vampire?")
+time.sleep(2)
+print("A frostbite!")
+time.sleep(1)
+
+print("")
+print("What do dentists call an astronaut's cavity?")
+time.sleep(2)
+print("A black hole!")
+time.sleep(1)
+
+print()
+print("Knock knock")
+input()
+print("Interrupting cow")
+input()
+print("Interrupting cow wh", end="")
+time.sleep(0.5)
+print("-MOOOO")
+
